bb9147b8c8c9312f4537d4c66f64527fb957602c
Marcus <139352735+sucrammal@users.noreply.github.com>
2025-01-15 01:58:23 +0800

    Finished Geochain entry

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -449,60 +449,299 @@ def decompose(time, emg, sampling_rate):
       image: "/images/geochain/geochainGameRoom.png",
       labels: ["Web Development"],
       fullContent: (
-        <section>
-          <img src="/images/geochain/geochainGameRoom.png" alt="game-room" className="w-6/12" />
-            <h3 className="font-semibold mt-4">Premise</h3>
+        <div>
+          <section>
+            <video className="w-full rounded-lg shadow-lg" controls>
+              <source src="/images/geochain/geochainDemoVid.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+              <h3 className="font-semibold mt-4">Premise</h3>
+              <ul className="list-disc ml-6">
+                  <li>Port a childhood dinnertime game into an online competitive format. The game is played by naming a Country, City, or State (CCS). The next person has to name another CCS starting with the last letter of the previously named CCS. This ends until someone fails to name one. </li>
+                </ul>
+              <h3 className="font-semibold mt-4">Responsibilities</h3>
+              <ul className="list-disc ml-6">
+                  <li>Learn the MERN stack and the Web Sockets API. </li>
+                  <li>Work alongisde Linh, a classmate, while using concurrency control (git) to track work, bugs, and patches.</li>
+                </ul>
+              <h3 className="font-semibold mt-4">Outcomes</h3>
+              <ul className="list-disc ml-6">
+                  <li>Designed the data structure for the CCS data to be stored and accessed from, and wrote logic to verify a user's CCS guess using RegEx. </li>
+                  <li>Designed the timer, player lives, map and marker, and end game condition logic. </li>
+                  <li>Used Web Sockets to to process the above logic in the server, and send concurrent updates to clients (players). </li>
+                </ul>
+              <h3 className="font-semibold mt-4">Technologies Used</h3>
+              <div className="mt-3 flex flex-wrap gap-1.5">
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                    Javascript
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  Python
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  Git
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  React
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  Express
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  Node
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  Websockets
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  HTML
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  CSS
+                </span>
+              </div>
+          </section>
+          <section>
+            <br></br>
+            <h2 className="font-bold mt-4 text-xl">Server-Side: Game Room Management</h2>  
+            <SyntaxHighlighter
+                  language="js"
+                  style={solarizedlight} // You can change this to any Prism.js theme
+                  customStyle={{
+                    padding: '1rem',
+                    borderRadius: '0.5rem',
+                    background: '#f5f2f0',
+                    fontSize: '0.8rem',
+                  }}
+                >
+                  {`io.on("connection", (socket) => {
+      console.log("A user connected:", socket.id);
+
+      // Handle user joining a room
+      socket.on("join-room", ({ gameId, nickname, timeLimit, lives }) => {
+          if (!gameRooms[gameId]) {
+              gameRooms[gameId] = {
+                  isStarted: false,
+                  users: [],
+                  locations: [],
+                  currentLetter: "A",
+                  currentTurnIndex: 0,
+                  timeLimit: timeLimit ?? 60, // Default to 60 seconds
+                  timeLeft: timeLimit ?? 60, // Set to the timeLimit
+                  timer: null, // Timer for the current turn
+                  lives: lives ?? 3, // Default to 3 lives
+                  guessedLocations: new Set(),
+                  isSolo: false, // Default to multiplayer
+              };
+          }
+      
+          const room = gameRooms[gameId];
+      
+          // Prevent users from joining if the game has already started
+          if (room.isStarted) {
+              socket.emit("game-started-error", { message: "The game has already started." });
+              return;
+          }
+      
+          // Add the user to the room
+          const user = { id: socket.id, name: nickname, lives: room.lives };
+          room.users.push(user);
+          socket.join(gameId);
+      
+          // ... 
+      
+          io.to(gameId).emit("update-users", room.users);
+          io.to(gameId).emit("update-turn", room.users[room.currentTurnIndex]);
+          io.to(gameId).emit("update-timeLeft", room.timeLeft);
+      });
+  });`}
+            </SyntaxHighlighter>
+            <br></br>
             <ul className="list-disc ml-6">
-                <li>Port a childhood dinnertime game into an online competitive format. The game is played by naming a Country, City, or state, and </li>
-              </ul>
-            <h3 className="font-semibold mt-4">Responsibilities</h3>
+              <li>Manage real-time multiplayer game rooms using websockets, including handling user connections, room creation, and state synchronization. </li>
+              <li>Implemented error handling to prevent users from joining a game that has already started.</li>
+            </ul>
+            <br></br>
+            <h2 className="font-bold mt-4 text-xl">Location Validation Logic</h2>
+            <SyntaxHighlighter
+                  language="js"
+                  style={solarizedlight} // You can change this to any Prism.js theme
+                  customStyle={{
+                    padding: '1rem',
+                    borderRadius: '0.5rem',
+                    background: '#f5f2f0',
+                    fontSize: '0.8rem',
+                  }}
+                >
+                  {`function validateLocation(input, gameId) { 
+    const inputName = input.toLowerCase().trim();
+
+    const keys = Array.from(locationsMap.keys());
+    const { bestMatch } = stringSimilarity.findBestMatch(inputName, keys);
+
+    if (bestMatch.rating > 0.95) { // Threshold for similarity set at 0.95
+        const location = locationsMap.get(bestMatch.target);
+
+        const guessedLocations = gameRooms[gameId]?.guessedLocations;
+        if (guessedLocations?.has(bestMatch.target)) {
+            return { success: false, message: "$bestMatch.target has already been guessed!" };
+        } else {
+            guessedLocations?.add(bestMatch.target);
+            return { success: true, location_data: location };
+        }
+    } else {
+        return { success: false, message: "$input" is not a valid location!" };
+    }
+}`}
+            </SyntaxHighlighter>
+            <br></br>
             <ul className="list-disc ml-6">
-                <li>Import and implement the data structure and logic behind verifying a correct location.</li>
-                <li>Help improve activation algorithm behind controlling the orthosis.</li>
-                <li>Read up on provided studies and concepts from PIs, and discuss research in weekly check-ins. </li>
-              </ul>
-            <h3 className="font-semibold mt-4">Outcomes</h3>
+              <li>The server normalizes the user's input and finds the closest match in the locationsMap using the string-similarity library.</li>
+              <li>If a close-enough match is found, we return the location name. The caller of this function can use locationMap (a hashmap) to access the lat and lon of the location to display on the map.</li>
+            </ul>
+            <br></br>
+            <h2 className="font-bold mt-4 text-xl">Timer and end condition logic</h2>
+            <SyntaxHighlighter
+                  language="js"
+                  style={solarizedlight} // You can change this to any Prism.js theme
+                  customStyle={{
+                    padding: '1rem',
+                    borderRadius: '0.5rem',
+                    background: '#f5f2f0',
+                    fontSize: '0.8rem',
+                  }}
+                >
+                  {`const startTurnTimer = (gameId) => {
+    const room = gameRooms[gameId];
+    if (!room) return;
+
+    if (room.timer) {
+        clearInterval(room.timer);
+        room.timer = null;
+    }
+
+    room.timeLeft = room.timeLimit;
+
+    room.timer = setInterval(() => {
+        io.to(gameId).emit("update-timeLeft", room.timeLeft);
+        room.timeLeft -= 1;
+
+        if (room.timeLeft <= 0) {
+            clearInterval(room.timer);
+
+            const currentTurnUser = room.users[room.currentTurnIndex];
+            if (currentTurnUser.lives > 0) {
+                currentTurnUser.lives -= 1;
+                io.to(gameId).emit("update-users", room.users);
+            }
+
+            const remainingPlayers = room.users.filter(user => user.lives > 0);
+            if (remainingPlayers.length === 1 && !room.isSolo) {
+                const winner = remainingPlayers[0];
+                io.to(gameId).emit("end-game", { 
+                    reason: "Last player standing", 
+                    winner: winner.name, 
+                    totalLocations: room.locations.length,
+                    isSolo: room.isSolo 
+                });
+                return;
+            } else if (room.isSolo && room.users[0].lives <= 0) {
+                io.to(gameId).emit("end-game", { 
+                    reason: "You lost all lives", 
+                    winner: "SOLO", 
+                    totalLocations: room.locations.length,
+                    isSolo: room.isSolo 
+                });
+                return;
+            } 
+            else if (!room.isSolo && room.users.length === 1) {
+                const winner = room.users[0];
+                io.to(gameId).emit("end-game", { 
+                    reason: "Players have disconnected in multiplayer game", 
+                    winner: winner.name, 
+                    totalLocations: room.locations.length,
+                    isSolo: room.isSolo 
+                });
+                return;
+            }
+
+            passTurn(gameId);
+        }
+        io.to(gameId).emit("update-timeLeft", room.timeLeft);
+    }, 1000);
+};    ;`}
+            </SyntaxHighlighter>
+            <br></br>
             <ul className="list-disc ml-6">
-                <li>Performed signal decomposition (Fast Fourier Transform) to analyze stroke patient EMG data patterns, applying MSE, cosine similarity, and averaging to distinguish patient actions, such as opening and closing a grasp, to control an orthosis. </li>
-                <li>Used t-SNE and k-nearest neighbours to classify live, incoming EMG data for the patient as open, close, or relax signals.</li>
-                <li>Computed power spectral densities and median frequency to identify downshifts in frequency, indicating fatigue.</li>
-                <li>Designed a variable-radius pulley system that allows for customizable cable tensioning and retraction lengths to suit patient's liking. </li>
-              </ul>
-            <h3 className="font-semibold mt-4">Technologies Used</h3>
-            <div className="mt-3 flex flex-wrap gap-1.5">
-              <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
-                Python
-              </span>
-              <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
-                SciPy
-              </span>
-              <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
-                NumPy
-              </span>
-              <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
-                Pandas
-              </span>
-              <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
-                Matplotlib
-              </span>
-              <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
-                Onshape
-              </span>
-            </div>
-        </section>
+              <li>Updating and synchronizing timer and lives data across clients, checking end-condition depending on solo/multiplayer. </li>
+              <li>Passes the turn if the current player runs out of time (or if a valid location is guessed). </li>
+            </ul>
+            <br></br>
+          </section>
+          <h2 className="font-bold mt-4 text-xl">Full Github repo here: {" "}
+            <a
+                    href="https://github.com/ldang04/CCS-Game"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-purple-600 hover:text-purple-800 underline decoration-purple-300 hover:decoration-purple-500 transition-colors"
+                    >
+                    Link
+                  </a>
+          </h2> 
+        </div>
+    
       ),
     },
     {
       id: 4,
       title: "Vectari (Now CredytU)",
       shortDescription: "Building LLM, AI classifiers for digital transaction complaints.",
-      tags: ["LLMs", "OpenAI", "NLP", "BERT", "Python", "Numpy", "pandas", "t-SNE", "RF", "K-means"],
-      image: "/images/vectari/vectariThumbnail",
+      tags: ["LLMs", "OpenAI", "NLP", "HPO", "BERT", "Python", "Numpy", "pandas", "t-SNE", "RF", "K-means"],
+      image: "/images/vectari/vectari3DKmeans.png",
       labels: ["Data Science"],
       fullContent: (
         <div>
           <section>
-          
+          <img src="/images/vectari/vectari3DKmeans.png" alt="K-means with tSNE" className="w-6/12" />
+          <h3 className="font-semibold mt-4">Premise</h3>
+              <ul className="list-disc ml-6">
+                  <li>One of two interns place on a team to classify 2 types of bank complaints: digital transactions and mortgages. </li>
+                  <li>Startup sells software to flag bank vulnerabilities based on consumer complaint trends to prevent compliance violations. </li>
+                </ul>
+              <h3 className="font-semibold mt-4">Responsibilities</h3>
+              <ul className="list-disc ml-6">
+                  <li>Use various ML algos and LLMs to build a classifier for digital transaction complaints. </li>
+                  <li>Work alongisde Harris, my co-intern in the team, helping with each other's code. </li>
+                  <li>Weekly check-ins with CTO for feedback and progress. Present researched findings at the end of the internship. </li>
+                </ul>
+              <h3 className="font-semibold mt-4">Outcomes</h3>
+              <ul className="list-disc ml-6">
+                  <li>Developed algorithms to categorise bank complaints and identify compliance violations using traditional NLP, Hugging Face ML models, OpenAI LLMs, and CO-STAR and TIDD-EC prompting frameworks. </li>
+                  <li>Fine-tuned GPT 3.5 to push classification precisions for certain complaint categories to 96% out of a 5-category dataset, distilling from GPT 4.0.  </li>
+                  <li>Worked on a more lightweight BERT model to classify complaints. </li>
+                  <li>Utilised Ray Tune for hyperparameter optimization, boosting SetFit classification accuracies from 75% to 90%. </li>
+                </ul>
+              <h3 className="font-semibold mt-4">Technologies Used</h3>
+              <div className="mt-3 flex flex-wrap gap-1.5">
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  Python
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  pandas
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  NumPy
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  SciPy
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  Matplotlib
+                </span>
+                <span className="px-2 py-1 text-s rounded-full bg-gray-100 text-gray-600">
+                  TensorFlow
+                </span>
+              </div>
           </section>
         </div>
       ),
