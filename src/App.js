212fdabdde06135ae3dc1dd912812731ed5ef60f
Marcus <139352735+sucrammal@users.noreply.github.com>
2025-01-26 23:21:37 -0500

    Build config

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import ProjectsPage from './components/ProjectsPage';
 import News from './components/News';
